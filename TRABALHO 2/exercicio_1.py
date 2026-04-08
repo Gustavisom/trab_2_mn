a098971eec234e858baa312fe2c6f8c005b68bd2
Gustavisom <gutobcmendes4@gmail.com>
2026-04-07 21:10:43 -0300

    add

--- TRABALHO 2/exercicio_1.py
+++ TRABALHO 2/exercicio_1.py
@@ -1,3 +1,5 @@
+from cProfile import label
+
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -16,34 +18,25 @@ def f(x):
 x = np.linspace(0, 2*math.pi, 100 )
 y = f(x)
 
-plt.figure()
-plt.plot(x, y, label='Função')
-plt.axhline(0, color='red', lw=0.5, ls='--')
-plt.title('Busca Incremental')
-plt.xlabel('x')
-plt.ylabel('f(x)')
-plt.legend()
-plt.grid()
+raizes = []
+for i in range(len(y)-1):
+    if y[i] * y[i+1] < 0:
+        raizes.append((x[i] + x[i+1]) / 2)
+        
+plt.figure(figsize=(10, 5))
+plt.plot(x, y, label=r'$f(x) = \sin(x) + \cos(5x) + \frac{1}{x}$', color='blue')
+plt.axhline(0, color='red', lw=0.8, ls='--', label=r'$f(x) = 0$')
+
+plt.title(r'$ f(x) = \sin(x) + \cos(5x) + \frac{1}{x}$ ', fontsize=14)
+plt.xlabel(r'$x$')
+plt.ylabel(r'$f(x)$')
+plt.ylim(-8, 8)  
+plt.legend(fontsize=11)
+plt.grid(alpha=0.4)
+plt.tight_layout()
+
+for r in raizes:
+    plt.plot(r, 0, 'go', markersize=5, label='Raiz')
+plt.tight_layout()
 plt.show()
 
-
-
-n = 100
-x = np.linspace(0, 2*np.pi, n)
-xb = []
-nb = 0
-
-
-for i in range(n-1):
-    xl = x[i]
-    xu = x[i+1]
-
-    if (f(xl) * f(xu) < 0):
-        nb += 1
-        xb.append([xl, xu])
-    else:    
-        print("Não há raiz entre", xl, "e", xu)
-
-print("xb = ", xb)
-print("y =", f(x))
-print("Número de intervalos com raiz:", nb)
